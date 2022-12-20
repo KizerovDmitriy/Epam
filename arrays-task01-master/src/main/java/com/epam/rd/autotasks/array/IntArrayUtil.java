5c9b61c4c1a94113900fcf2bd068e82a5b860cef
KizerovDmitriy <113462277+KizerovDmitriy@users.noreply.github.com>
2022-12-20 10:25:53 +0200

    Upload solved 5 tasks

---
+++ arrays-task01-master/src/main/java/com/epam/rd/autotasks/array/IntArrayUtil.java
@@ -0,0 +1,50 @@
+package com.epam.rd.autotasks.array;
+
+import java.util.Arrays;
+
+public class IntArrayUtil {
+
+	public static void swapEven(int[] array) {
+		if (array==null || array.length==0){
+
+		}
+		else {
+			for (int i = 0; i < array.length/2; i++) {
+				if (array[i]%2==0 && array[array.length-i-1]%2==0){
+					int tmp=array[i];
+					array[i]=array[array.length-1-i];
+					array[array.length-1-i]=tmp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		{
+			int[] array = null;
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] {};
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] { 10, 5, 3, 4 };
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] { 100, 2, 3, 4, 5 };
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] { 100, 2, 3, 45, 33, 8, 4, 54 };
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+	}
+
+}
---
+++ pizza-split/src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -0,0 +1,27 @@
+package com.epam.rd.autotasks.pizzasplit;
+
+import java.util.Scanner;
+
+public class PizzaSplit {
+    public static void main(String[] args) {
+
+        //Write a program, reading number of people and number of pieces per pizza and then
+        //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOfQuests = scanner.nextInt();
+        int piecesOfPizza = scanner.nextInt();
+        int pizzaCount = 1;
+        int addPizza = piecesOfPizza;
+
+        while (true) {
+            if (piecesOfPizza % numberOfQuests == 0) {
+                break;
+            } else {
+                piecesOfPizza += addPizza;
+                pizzaCount++;
+            }
+        }
+        System.out.println(pizzaCount);
+    }
+}
